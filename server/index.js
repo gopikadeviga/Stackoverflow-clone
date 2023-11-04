8547c826aa9e20fd68990d957c6bb8c2d0ef55c3
Gopika Devi <gopikadeviga@gmail.com>
2023-11-04 14:14:44 +0400

    commit

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 //cors is for eliminating the error(Cross origin) thrown while the request is send between the servers
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app = express(); //creating express server
+dotenv.config()
 app.use(express.json({limit: '30mb',extended: "true"}))
 app.use(express.urlencoded({limit: "30mb",extended: "true"}))
 app.use(cors());
